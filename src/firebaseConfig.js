b49a4a829db7a9d7974c2b0046fe6da4195eafb8
leslieturai <turaileslie@gmail.com>
2025-02-22 10:23:19 -0600

    Code, save CSS, commented

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+// Importing necessary firebase module functions
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js"
+
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut  } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
 
 import { getDatabase, ref, child, get, set, remove } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js";
 
-
-
-
+///
+// Configuring firebase instance
 const firebaseConfig = {
     apiKey: "AIzaSyAKsJC-oY99grk3vOF1jqnGG2Xhpaxcyn8",
     authDomain: "midterm-8c698.firebaseapp.com",
@@ -19,9 +20,13 @@ const firebaseConfig = {
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+// Variables for database and authorization
 const db = getDatabase(app)
+const dbRef = ref(db)
+const auth = getAuth();
 
+// Variables for testing, but also to show example structure of the actual budget object
+// userBudget2 demonstrates a budget that has content
 var userBudget2 = {
   "userBudget": {
       "setBudget": 500,
@@ -44,51 +49,40 @@ var userBudget2 = {
       ]
   }
 }
-  var userBudget = {
-    "userBudget": {
-        "setBudget": 0,
-        "expenses": [ 
+// No content example - this will be read as null by the app and treated as such by firebase as well
+var userBudget = {
+  "userBudget": {
+      "setBudget": 0, // setBudget being zero means no data
+      "expenses": [ // As expenses is empty, it will not appear in the database, making the budget null
 
-        ]
-    }
+      ]
   }
+}
 
-
-const dbRef = ref(db)
-
-
-
-
-
-const auth = getAuth();
-
-
+// Function called in index.html to handle logging in
 export async function handleGoogleLogIn () {
   const provider = new GoogleAuthProvider();
 
   signInWithPopup(auth, provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
+      const credential = GoogleAuthProvider.credentialFromResult(result)
+      const token = credential.accessToken
       // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
+      const user = result.user
       window.location.href = "./utils/pages/dashboard.html"
     }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
+      // Handle Errors
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used
+      const email = error.customData.email
       // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
+      const credential = GoogleAuthProvider.credentialFromError(error)
       console.log(errorMessage)
-      // ...
-    });
-};
-
+    })
+}
+// Function to handle signing out - called in dashboard.html
 export async function handleGoogleSignOut () {
   signOut(auth)
     .then((result) => {
@@ -100,21 +94,21 @@ export async function handleGoogleSignOut () {
     })
 }
 
-
+// Async function to retrieve the budget if it exists
 export async function getBudget() {
   try {
     const snapshot = await get(child(dbRef, "/0"))
     if (!snapshot.val().userBudget.expenses) {
       console.log("no expenses")
-      return 
+      return // If no expenses, return nothing
     }
-    return snapshot.val()
+    return snapshot.val() // Otherwise, we return the budget object
   } catch (err) {
     console.error(err)
     return null
   }
 }
-
+// Function to delete budget, used to reset 
 export async function deleteSavedBudget () {
   try {
     const pathRef = ref(db, "/0"); 
@@ -124,7 +118,7 @@ export async function deleteSavedBudget () {
     console.error("Error deleting data:", error)
   } 
 } 
-
+// Function to set the budget after deleteSavedBudget() is called
 export async function setBudget (newBudget) {
   try {
     set(ref(db, "0"), newBudget )
@@ -133,12 +127,11 @@ export async function setBudget (newBudget) {
   }
 }
 
+// Testing functions to clear the database and write test data
 //deleteSavedBudget()
 //setBudget(userBudget2)
 
-
-// Test comment
-
+// Function to delete one expense, found in Real Time Database via its id
 export async function deleteDBexpense (id) {
   try {
     const pathRef = ref(db, "/0/userBudget/expenses/" + id); 
--- utils/expenseFunctions.js
+++ utils/expenseFunctions.js
@@ -1,6 +1,10 @@
+// Importing function to create new expense items in HTML via JS
 import addExpenseItem from "./expenseItem.js"
+
+// Importing relevant firebase functions
 import { getBudget, deleteSavedBudget, setBudget } from "../src/firebaseConfig.js"
 
+// Reference for where the expenses will be appended
 const budgetItemContainer = document.querySelector("#content-row-two")
 
 // Budget input field
@@ -23,29 +27,19 @@ const saveExpBtn = document.querySelector("#save-expense-btn")
 
 const errorDisplay = document.querySelector("#error-display")
 
-// Remove expense button
+// Remove expense buttons
 var removeExpBtn = []
 
-var budget;
+var budget; // Local copy of budget data, used as redundancy and if there is no object to retrieve from database
 
-var budgetArray = []
+var budgetArray = [] // Retrieved budget is assigned to this variable directly if it exists
 
-
-
-
-
-/* function renderExpenses () {
-    for (let i = 0; i < budgetArray.expenses.length; i++) {
-        addExpenseItem(budgetArray.expenses[i].label, budgetArray.expenses[i].cost, budgetItemContainer)
-    }
-} */
-
+// Function to clear all expenses in HTML, simply deleted, used to reset the list if necessary before rerendering
 function clearExpenses () {
-    const elements = document.getElementsByClassName(".expense-item")
     document.querySelectorAll(".expense-item").forEach(element => element.remove())
 }
 
-// Function to calculate remaining cash
+// Functions to calculate remaining cash
 function calculateRemainder (arr) {
     let tempSum = 0
     for (let i = 0; i < arr.length; i++) {
@@ -62,63 +56,72 @@ function calculateTotalExpenses (arr) {
     expenseTotal.innerHTML = "Total Expenses: " + "$" + tempTotal
 }
 
+// Function to render expenses
 function renderExpenses() {
+    // Currently rendered expenses are removed
     document.querySelectorAll(".expense-item").forEach(element => element.remove())
-
+    // Creating, appending, and rendering a new expense item for every element in the expenses array of the budget object
     for (let i = 0; i < budgetArray.expenses.length; i++) {
         addExpenseItem(budgetArray.expenses[i].label, budgetArray.expenses[i].cost, budgetItemContainer);
     }
 
-    attachRemoveEventListeners(); // Reattach event listeners after rendering
-    calculateTotalExpenses(budgetArray.expenses)
+    attachRemoveEventListeners(); // Reattach event listeners for remove buttons after rendering
+    calculateTotalExpenses(budgetArray.expenses) // Calculating total expenses and displaying the amount
 }
 
+// Function to attach and handle event listeners for the remove remove 
 function attachRemoveEventListeners() {
-    removeExpBtn = document.querySelectorAll(".expense-remove-btn");
-
+    removeExpBtn = document.querySelectorAll(".expense-remove-btn") // nodeList reference for all remove buttons
+    // Attach an event listener on each button
     removeExpBtn.forEach((element, i) => {
         element.addEventListener("click", () => {
+            // The nodeList and expenses array indices are backwards when compared, so the correct index of the task
+            // which corresponds to its expense index needs to be calculated
+            // The amount of buttons (length of nodeList converted to Array) - the index of the element in the spreaded nodeList - 1 (to offset zero-index)
             let tempIndex = ([...removeExpBtn].length - [...removeExpBtn].indexOf(element) - 1)
+            /* 
+                Ex: 3rd element in the nodeList out of 4 has its remove button clicked,
+                4 - 2 - 1 = 1
+                1 is that corresponding expense's index representation in the expenses array in budgetArray, so it is spliced and the expenses rerendered below
+            */
 
             budgetArray.expenses.splice(tempIndex, 1);
-            renderExpenses(); // Rerender the list after removing an item
+            renderExpenses() // Rerender the list after removing an item
             calculateTotalExpenses(budgetArray.expenses)
-        });
-    });
+        })
+    })
 }
 
+// Function to quickly set displays to zero if there is no budget
 function handleNoBudget () {
     expenseBudget.innerHTML = "Budget:" + " " + "$" + 0
     remainder.innerHTML = "Remainder: " + " " + "$" + 0
     expenseTotal.innerHTML = "Total Expenses: " + "$" + 0
 }
 
+// Async function to check if there is a budget
 async function fetchBudget() {
     budget = await getBudget();
-
     if (!budget) {
-        console.log("user has no budget")
-
+        // Intializing displays to zero if not found
         handleNoBudget()
         return
     }
-    
-    
+    // Else, budgetArray gets a copy of the returned data
     budgetArray = budget.userBudget;
-    renderExpenses();
-
+    renderExpenses() // Data is rendered
+    // Budget, expenses, and remainder are calculated
     expenseBudget.innerHTML = "Budget:" + " " + "$" + budget.userBudget.setBudget
     remainder.innerHTML = "Remainder: " + " " + "$" + String(budget.userBudget.setBudget - calculateRemainder(budgetArray.expenses))
     calculateTotalExpenses(budgetArray.expenses)
 }
-
+// Calling fetchBudget
 fetchBudget();
 
 // Functionality to add expenses
 function addExpense (_id, _label, _cost, _budget) {
     if (newExpName.value && newExpCost) {
-        console.log("valid expense")
-
+        // As long as the expense has content, a new object is simply pushed to budgetArray.expenses
         budgetArray.expenses.push(
             {
                 id: _id,
@@ -126,22 +129,12 @@ function addExpense (_id, _label, _cost, _budget) {
                 cost: Number(_cost)
             }
         )
-
-        console.log(budgetArray)
-
     } else {
         console.log("Error")
         return
     }
-
-    
 }
-
-
-
-
-// budgetArray.map((record) => console.log(record.cost))
-
+// Exporting functions for dashboard.html to handle budget data once it is retrieved
 export function getExpenseLabels () {
     let tempLabels = []
     for (let i = 0; i < budgetArray.length; i++) {
@@ -158,48 +151,11 @@ export function getCosts () {
     return tempCosts
 }
 
+// Button Event Handlers below
 
-export function testBudget () {
-    return budgetArray
-}
-
-
-
-
-
-
-
-
-
-
+// Event listener for handling budget input
 budgetInput.addEventListener("input", (event) => {
-    
-
     event.preventDefault()
-
-    budget.userBudget.setBudget = Number(budgetInput.value)
-
-    expenseBudget.innerHTML = "Budget: " + "$" + budget.userBudget.setBudget
-    remainder.innerHTML = "Remainder: " + " " + "$" + String(budget.userBudget.setBudget - calculateRemainder(budgetArray.expenses))
-
-
-})
-
-addExpBtn.addEventListener("click", (event) => {
-    event.preventDefault()
-
-    // Expense error handling
-   if (newExpName.value == "") {
-        errorDisplay.innerHTML = "Error: please enter an expense name"
-        errorDisplay.setAttribute("class", "error")
-        return
-    } else if (newExpCost.value == "" || newExpCost.value < 0) {
-        errorDisplay.innerHTML = "Error: please enter an expense cost"
-        errorDisplay.setAttribute("class", "error")
-        return
-    }
-
-
     if (!budget) {
         budget = {
             "userBudget": {
@@ -209,47 +165,89 @@ addExpBtn.addEventListener("click", (event) => {
                 ]
             }
           }
+        // Setting setBudget to user input directly
+        budget.userBudget.setBudget = Number(budgetInput.value)
+        // Setting bugdetArray to reference
         budgetArray = budget.userBudget
+        // Displaying and updating budget and remainder as they type
+        expenseBudget.innerHTML = "Budget: " + "$" + budget.userBudget.setBudget
+        remainder.innerHTML = "Remainder: " + " " + "$" + String(budget.userBudget.setBudget - calculateRemainder(budgetArray.expenses))
     }
+
+    // Displaying and updating budget and remainder as they type
+    expenseBudget.innerHTML = "Budget: " + "$" + budget.userBudget.setBudget
+    remainder.innerHTML = "Remainder: " + " " + "$" + String(budget.userBudget.setBudget - calculateRemainder(budgetArray.expenses))
+})
+
+// Event listener for "Add" button to add an expense
+addExpBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    // Error handling
+    // Error messages are displayed via changing the class
+    // of an element and modifying its displayed message
+   if (newExpName.value == "") {
+        errorDisplay.innerHTML = "Error: please enter an expense name"
+        errorDisplay.setAttribute("class", "error")
+        return
+    } else if (newExpCost.value == "" || newExpCost.value < 0) {
+        errorDisplay.innerHTML = "Error: please enter an expense cost"
+        errorDisplay.setAttribute("class", "error")
+        return
+    }
+    // If there is no budget retrieved - or it doesn't exist at this point for some reason,
+    // create the budget manually as a disposable reference (see firebaseConfig.js for data structure examples)
+    if (!budget) {
+        budget = {
+            "userBudget": {
+                "setBudget": 0,
+                "expenses": [ 
+        
+                ]
+            }
+          }
+          // Setting bugdetArray to reference
+          budgetArray = budget.userBudget
+    }
+    // Otherwise, add the expense
     addExpense(budgetArray.expenses.length + 1, newExpName.value, newExpCost.value)
+    // Delete rendered expenses
     clearExpenses()
+    // Rerender expenses
     for (let i = 0; i < budgetArray.expenses.length; i++) {
         addExpenseItem(budgetArray.expenses[i].label, budgetArray.expenses[i].cost, budgetItemContainer)
     }
+    // Update event listeners of rerendered list for their remove buttons
     attachRemoveEventListeners()
-
+    // Setting error display to a success message
     errorDisplay.innerHTML = "Expense added!"
     errorDisplay.setAttribute("class", "success")
-
+    // Total expeneses recalculated
     calculateTotalExpenses(budgetArray.expenses)
-
+    // Onput fields cleared
     budgetInput.value = ""
     newExpName.value = ""
-    newExpCost.value = ""
-
+    newExpCost.value = ""   
+    // Setting the error/success message to disappear after 5 seconds via another class change
     setTimeout(function () {
         errorDisplay.innerHTML = ""
         errorDisplay.setAttribute("class", "hidden")
     }, 5000)
-    
 })
 
+// Event listener for the "Save" button for saving data to firebase
 saveExpBtn.addEventListener("click", (event) => {
-
-
-
     event.preventDefault()
-    //
+    // Deleting data saved already
+    // (Data the user did not delete is still saved in both budget and BudgetArray)
+    deleteSavedBudget()
+    // Saving the new budget object to firebase
+    setBudget(budget)
+    // Clearing rendered expenses
+    clearExpenses()
+    // Fetching the saved data from the database directly to refresh
+    fetchBudget()
+    // Resetting input fields
     budgetInput.value = ""
     newExpName.value = ""
     newExpCost.value = ""
-
-    //
-
-    deleteSavedBudget()
-    setBudget(budget)
-    //
-    clearExpenses()
-    fetchBudget()
-
 })
